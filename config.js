4f6f10ebea035bb4cb9422118c51d1844b540510
DominicDev <s0l0estudi4r@gmail.com>
2025-12-12 11:12:02 +0100

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'LENNY KRAVITZ ';
+export let nameSongs = [
+	"FLY AWAY", /*1*/
+	"LOW", /*2*/
+	"THE CHAMBER", /*3*/
+	"BELIEVE IN ME", /*4*/
+	"I BELONG TO YOU", /*5*/
+	"IT AIN'T OVER 'TIL IT'S OVER", /*6*/
+	"AGAIN", /*7*/
+	"ARE YOU GONNA GO MY WAY", /*8*/
+	"AMERICAN WOMAN", /*9*/
+	"LET LOVE RULE", /*10*/
+	"CAN'T GET YOU OFF MY MIND", /*11*/
+	"YOU WERE IN MI HEART", /*12*/
+	"LET’S GET HIGH", /*13*/
+	"IF I COULD FALL IN LOVE", /*14*/
+	"DANCIN' TIL DAWN", /*15*/
+	"NEW YORK CITY", /*16*/
+	"THE PLEASURE AND THE PAIN", /*17*/
+	"STILLNESS OF HEART", /*18*/
+	"HEAVEN HELP", /*19*/
+	"SUGAR", /*20*/
+]
